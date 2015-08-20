1432d5a59a957b277e38299ef64c8f3eddd2dec5
eoglethorpe <ewanogle@gmail.com>
2015-08-20 10:24:28 +0545

    change to etl, add setup and init

---
+++ shelter/ETL/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='etl',
+      version='0.1',
+      description='distribution etl',
+      author='Ewan Oglethorpe',
+      author_email='ewanogle@gmail.com',
+      packages = ['etl'], 
+)
