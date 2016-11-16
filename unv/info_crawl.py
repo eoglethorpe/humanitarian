8876e41d2383eec4e10f4da2542d148b3f2fc999
eoglethorpe <ewanogle@gmail.com>
2016-11-16 13:41:28 +0545

    more

--- unv/info_crawl.py
+++ unv/info_crawl.py
@@ -1,25 +1,31 @@
 """crawl un info to get all reports"""
 from lxml import html
-import requests
+import grequests
 
 
 def find_count():
-    base= 'http://un.info.np/Net/NeoDocs/View/'
+    br = 0
+    gd = 0
+    bd = 0
 
-    good = 0
-    bad = 0
-    for i in xrange(8000):
-        cur = base + str(i)
-        req = requests.get(cur)
+    BASE = 'http://un.info.np/Net/NeoDocs/View/'
+    rs = (grequests.get(BASE + str(i)) for i in xrange(5000))
+    reqs = grequests.map(rs)
+
+    def exception_handler(request, exception):
+        global br
+        br +=1
+
+    for req in reqs:
         if req.text.rfind('Invalid Operation !\r\n') == -1:
-            bad += 1
+            gd += 1
         else:
-            good += 1
+            bd += 1
 
-        print 'bad: ' + str(bad)
-        print 'good: ' + str(good)
-        print
-        break
+    print 'bad: ' + str(bd)
+    print 'good: ' + str(gd)
+    print 'broken: ' + str(br)
+    print
 
 if __name__ == '__main__':
     find_count()
