c24d414885681f87f1c6d485c259bf1b0e405bd7
Ewan Oglethorpe <oglethorpe.ewan@gmail.com>
2018-06-23 17:31:07 -0500

    fix desinv loc

--- shelter/sohss/c0d3/desinv_pull.py
+++ shelter/sohss/c0d3/desinv_pull.py
@@ -3,7 +3,7 @@ import pandas as pd
 """read in desinv historical data, create ISOz"""
 
 def pull():
-    LOC = '../desinventar.csv'
+    LOC = '../d0cz/desinventar.csv'
     desinv = pd.read_csv(LOC)
     # desinv.columns = ['div_' + v.lower().replace(' ', '_') for v in desinv.columns]
     desinv['div_uid'] = desinv['div_country_code'] + desinv['div_year'].map(str)
